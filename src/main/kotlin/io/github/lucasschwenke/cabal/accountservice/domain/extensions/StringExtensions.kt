8a2f27ea9098e1dee36c0e5e845797590c6320e7
Lucas Paixao <lucasspxo@gmail.com>
2020-10-25 19:01:29 -0300

    fix: fixed string extension for MD5

--- src/main/kotlin/io/github/lucasschwenke/cabal/accountservice/domain/extensions/StringExtensions.kt
+++ src/main/kotlin/io/github/lucasschwenke/cabal/accountservice/domain/extensions/StringExtensions.kt
@@ -3,7 +3,6 @@ package io.github.lucasschwenke.cabal.accountservice.domain.extensions
 import java.security.MessageDigest
 import javax.xml.bind.DatatypeConverter
 
-fun String.toSHA1() = hashString("SHA-1", this)
 fun String.toMD5() = hashString("MD5", this)
 
 private fun hashString(type: String, input: String): String {
--- src/main/resources/sql/auth/insert_auth.sql
+++ src/main/resources/sql/auth/insert_auth.sql
@@ -1,2 +1,2 @@
 INSERT INTO cabal_auth_table(ID, Password, Email, Hash, Chave, Login, AuthType, Perg, IdentityNo)
-VALUES (:id, HASHBYTES('SHA1 ', :password), :email, :hash, :key, :login, :authType, :perg, :identityNo);
+VALUES (:id, HASHBYTES('SHA1', :password), :email, :hash, :key, :login, :authType, :perg, :identityNo);
